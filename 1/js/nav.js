5a42190d2894a5ce3d46acf37c330299cf626a7e
Patrick Mk <trickmartpat77@gmail.com>
2020-06-23 00:41:29 +0700

    template jadi

--- 1/js/nav.js
+++ 1/js/nav.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
     
     // Activate sidenav
-    const elems = document.querySelector(".sidenav");
+    const elems = document.querySelectorAll(".sidenav");
     M.Sidenav.init(elems);
     loadNav();
 
     // Load page content
     let page = window.location.hash.substr(1);
-    if(page == "") page = "home";
+    if(page == "") page = "home"; 
     loadPage(page);
+
 
     function loadNav(){
         const xhttp = new XMLHttpRequest();
@@ -22,10 +23,10 @@ document.addEventListener("DOMContentLoaded", function(){
                 });
 
                 // Daftarkan event listener pada setiap tautan menu
-                document.querySelectorAll(".topnav, .sidenav").forEach(elm => {
+                document.querySelectorAll(".topnav a, .sidenav a").forEach(elm => {
                     elm.addEventListener("click", event => {
                         // Tutup Sidenav
-                        const sidenav = document.querySelector(".sidenav");
+                        let sidenav = document.querySelector(".sidenav");
                         M.Sidenav.getInstance(sidenav).close();
 
                         // Muat konten yang dipanggil
@@ -36,26 +37,37 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         };
 
-        xhttp.open("GET", "pages/nav.html", true);
+        xhttp.open("GET", "nav.html", true);
         xhttp.send();
     }
 
     function loadPage(page){
         const xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function(){
-            if(this.state == 4){
-                const content = document.querySelector("#body-content");
+            if(this.readyState == 4){
+                let content = document.querySelector("#body-content");
                 if(this.status == 200){
                     content.innerHTML = xhttp.responseText;
+                    if(page == 'home'){
+                        // Slider
+                        const slider = document.querySelectorAll(".slider");
+                        M.Slider.init(slider, {
+                            indicators: false,
+                            height: 200,
+                            duration: 600,
+                            interval: 3000
+                        });
+                    }
                 } else if(this.status == 404){
                     content.innerHTML = "<p>Halaman tidak dapat ditemukan!</p>";
                 } else {
                     content.innerHTML = "<p>Ups.. Halaman tidak dapat diakses.</p>";
                 }
             }
+            console.log(page);
         };
 
-        xhttp.open("GET", "pages/" + page + ".html", true);
-        xhttp.send()
+        xhttp.open("GET",`/pages/${page}.html`, true);
+        xhttp.send();
     }
 });
